bff7c2eb2ee00791fc8f30543e14107d0b2d4bfa
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-08-22 15:39:44 -0400

    completed blog date archiving and display

--- front-page.php
+++ front-page.php
@@ -16,9 +16,23 @@
     <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
     <?php 
+      $today = date('Ymd');
       $homepageEvents = new WP_Query(array(
-        'posts_per_page' => 2,
-        'post_type' => 'event'
+        'posts_per_page' => -1,
+        'post_type' => 'event',
+        'meta_key' => 'event_date',
+        'orderby' => 'meta_value_num',
+        'order' => 'ASC',
+        'meta_query' => array(
+          array(
+            'key' => 'event_date',
+            'compare' => '>=',
+            'value' => $today,
+            'type' => 'numeric'
+          )
+        )
+        // this meta_query is allowing us to only display dates that are in the future, 
+        // all events that are in the past will not be displayed
       ));
       while($homepageEvents->have_posts()) {
         $homepageEvents->the_post(); ?>
